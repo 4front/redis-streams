2abb3331392b913d7f27efce423cdf024467c75d
David Von Lehman <david.vonlehman@gmail.com>
2016-05-10 20:48:47 -0700

    Support for no maxAge Update npm dependencies Eliminate lodash dependency

--- index.js
+++ index.js
@@ -2,50 +2,47 @@ var crypto = require('crypto');
 var redisWStream = require('redis-wstream');
 var redisRStream = require('redis-rstream');
 var through2 = require('through2');
-var _ = require('lodash');
 
 module.exports = function(redis) {
-  _.extend(redis.RedisClient.prototype, {
-    writeThrough: function(key, maxAge) {
-      var self = this;
-      var tempSuffix = crypto.randomBytes(15).toString('base64');
-      var redisStream = redisWStream(this, key, { tempKeySuffix: tempSuffix });
+  redis.RedisClient.prototype.writeThrough = function(key, maxAge) {
+    var self = this;
+    var tempSuffix = crypto.randomBytes(15).toString('base64');
+    var redisStream = redisWStream(this, key, { tempKeySuffix: tempSuffix });
 
-      return through2.obj(function(chunk, enc, callback) {
-        var out = this;
+    return through2.obj(function(chunk, enc, callback) {
+      var out = this;
 
-        redisStream._write(chunk, enc, function() {
-          // https://github.com/jeffbski/digest-stream/blob/master/lib/digest-stream.js#L13
-          out.push(chunk);
-          callback();
-        });
-      }, function(cb) {
-        redisStream.end(function() {
-          // Set the expiry of the cache entry. Need to do this in the callback
-          // to ensure that the key was renamed to the actual value.
-          self.expire(key, maxAge);
-
-          // Delete the temp key in case it is still there. This shouldn't be necessary,
-          // the redis-wstream is supposed to rename the key
-          self.del(key + tempSuffix);
-          cb();
-        });
+      redisStream._write(chunk, enc, function() {
+        // https://github.com/jeffbski/digest-stream/blob/master/lib/digest-stream.js#L13
+        out.push(chunk);
+        callback();
       });
-    },
+    }, function(cb) {
+      redisStream.end(function() {
+        // Set the expiry of the cache entry. Need to do this in the callback
+        // to ensure that the key was renamed to the actual value.
+        if (maxAge) self.expire(key, maxAge);
 
-    writeStream: function(key, maxAge) {
-      var self = this;
-      var tempSuffix = crypto.randomBytes(15).toString('base64');
+        // Delete the temp key in case it is still there. This shouldn't be necessary,
+        // the redis-wstream is supposed to rename the key
+        self.del(key + tempSuffix);
+        cb();
+      });
+    });
+  };
 
-      return redisWStream(this, key, {tempKeySuffix: tempSuffix})
-        .on('finish', function() {
-          self.expire(key, maxAge);
-          self.del(key + tempSuffix);
-        });
-    },
+  redis.RedisClient.prototype.writeStream = function(key, maxAge) {
+    var self = this;
+    var tempSuffix = crypto.randomBytes(15).toString('base64');
 
-    readStream: function(key) {
-      return redisRStream(this, key);
-    }
-  });
+    return redisWStream(this, key, {tempKeySuffix: tempSuffix})
+      .on('finish', function() {
+        if (maxAge) self.expire(key, maxAge);
+        self.del(key + tempSuffix);
+      });
+  };
+
+  redis.RedisClient.prototype.readStream = function(key) {
+    return redisRStream(this, key);
+  };
 };
